51fa0cdd30ddd3d57357a8ca90f5d31a476f107c
Throsby <throsbywells@gmail.com>
2022-03-06 23:26:50 -0500

    commit tree bd00abf51d827efa1c09810ec095670b49ddb22c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+document.querySelector('main').remove();
+const element = document.createElement("h1");
+document.body.append(element);
+const h1 = document.getElementById("h1");
+h1.className = "victory";
